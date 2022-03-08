74f24b5a86b6da335456d9eb445159fcbe64aed9
David Valle <52580367+dvallemo@users.noreply.github.com>
2022-03-07 19:08:00 -0500

    Update operatorOverload.cpp

--- Operator Overload/operatorOverload.cpp
+++ Operator Overload/operatorOverload.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Complex{
+class Complex{ //defines complex class
 private:
-    int real, img;
+    int real, img; //defines the member varaibles
 public:
-    Complex(){
-        real = 0;
-        img = 0;
-    }
-    Complex(int r, int i){
+    Complex() = default; //sets a default consturctor which has all member varaibles set to zero
+    
+    Complex(int r, int i){ //constructor parametrizes the memeber variables
         real = r;
         img = i;
     }
 
-    void print(){
+    void print(){ //method that prints out the real and img compnenet of the complex number sum
         cout << real << " + " << img << "j" << endl;
 
     }
     //operator overloading syntax
     Complex operator +(Complex c)
     {
-        Complex temp;
-        temp.real = real + c.real;
+        Complex temp; //sets a temporary object to return the complex of complex type
+        temp.real = real + c.real; //adds teh real components together of all c instances
         temp.img = img + c.img;
 
         return temp;
@@ -31,10 +29,10 @@ public:
 };
 
 int main() {
-    Complex c1(5,4);
+    Complex c1(5,4); //sets first complex number instance to 5+4j
     Complex c2(2, 5);
     Complex c3(1, 1);
-    Complex c4;
+    Complex c4; //object to store all the instances in 
 
     c4 = c1 + c2 +c3;
     c4.print();
